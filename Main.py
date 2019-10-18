a7eaa5a6480b48cbf44e7dd401b7545f0edbfa9c
daniel <ssdaniel100@gmail.com>
2019-10-18 20:35:54 +0300

    init

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
-from hotel_code import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+app.run(host='0.0.0.0')
